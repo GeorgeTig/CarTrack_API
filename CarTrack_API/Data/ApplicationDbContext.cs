c60aa431bb5aa274d891532ce015618bea9e77f4
GeorgeTig <147520898+GeorgeTig@users.noreply.github.com>
2025-02-28 15:57:35 +0200

    DbContext
    Added the package and the class to create and manipulate an database in postgresSQL

---
+++ CarTrack_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using CarTrack_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarTrack_API.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+    {
+        
+    }
+    
+    public DbSet<User> Users { get; set; }
+    public DbSet<Vehicle> Vehicles { get; set; }
+    public DbSet<VehiclePapers> VehiclePapers { get; set; }
+    public DbSet<Engine> Engines { get; set; }
+    public DbSet<Mechanic_Service> Mechanic_Services { get; set; }
+    public DbSet<MaintenanceRecord> MaintenanceRecords { get; set; }
+    public DbSet<Appointment> Appointments { get; set; }
+    public DbSet<Service> Services { get; set; }
+    public DbSet<UserRole> UserRoles { get; set; }
+    public DbSet<Notification> Notifications { get; set; }
+}
