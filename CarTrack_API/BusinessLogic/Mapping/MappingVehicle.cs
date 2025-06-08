f3d024c888a79143fa5ae3886526886b256b4d12
GeorgeTig <147520898+GeorgeTig@users.noreply.github.com>
2025-06-08 22:29:12 +0300

    small changes

--- CarTrack_API/BusinessLogic/Mapping/MappingVehicle.cs
+++ CarTrack_API/BusinessLogic/Mapping/MappingVehicle.cs
@@ -5,22 +5,46 @@ namespace CarTrack_API.BusinessLogic.Mapping;
 
 public static class MappingVehicle
 {
-    public static List<VehicleResponseDto> ToListVehicleResponseDto(this List<Vehicle> vehicle)
+    public static List<VehicleResponseDto> ToListVehicleResponseDto(this List<Vehicle> vehicles)
     {
         var vehicleResponseDtos = new List<VehicleResponseDto>();
-        foreach (var item in vehicle)
+    
+        // Verificăm dacă lista primită nu este null
+        if (vehicles == null)
         {
-            var vehicleResponseDto = new VehicleResponseDto
-            {
-                Id = item.Id,
-                Vin = item.VehicleInfo.Vin,
-                Series = item.VehicleModel.SeriesName,
-                Year = item.VehicleModel.Year
-            };
-            vehicleResponseDtos.Add(vehicleResponseDto);
+            return vehicleResponseDtos;
         }
-        
 
+        foreach (var vehicle in vehicles)
+        {
+            // Verificăm dacă vehiculul în sine nu este null
+            if (vehicle != null)
+            {
+                // Cream un DTO cu valori default (goale)
+                var vehicleResponseDto = new VehicleResponseDto
+                {
+                    Id = vehicle.Id,
+                    Vin = "N/A", // Valoare default
+                    Series = "Unknown Series", // Valoare default
+                    Year = 0 // Valoare default
+                };
+
+                // Populăm câmpurile doar dacă obiectele relaționate nu sunt null
+                if (vehicle.VehicleInfo != null)
+                {
+                    vehicleResponseDto.Vin = vehicle.VehicleInfo.Vin;
+                }
+
+                if (vehicle.VehicleModel != null)
+                {
+                    vehicleResponseDto.Series = vehicle.VehicleModel.SeriesName;
+                    vehicleResponseDto.Year = vehicle.VehicleModel.Year;
+                }
+            
+                vehicleResponseDtos.Add(vehicleResponseDto);
+            }
+        }
+    
         return vehicleResponseDtos;
     }
 
